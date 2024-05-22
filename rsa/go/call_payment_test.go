4829d16852a20b48f3499a4c7b74c63ac2275bdc
luo-dahui <934803813@qq.com>
2024-05-22 19:02:07 +0800

    call payment interface

--- rsa/go/call_payment_test.go
+++ rsa/go/call_payment_test.go
@@ -17,14 +17,42 @@ import (
 )
 
 const (
-	httpUrl    = ""
+	httpUrl    = "http://192.168.3.46:10011/interface/api"
 	method     = "POST"
-	appKey     = "123456"
+	appKey     = "test"
 	secret     = "123456"
 	cryptoHash = crypto.SHA256
 	keyFmt     = PKCS8
-	priKey     = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCyDNjeeOjLdoR7\nkOdi5BiS5C8fVvoLoTSkEGNnNJXdJiZ2mPXG5av1DLUT8gvaV0NJmukeG/BXEUHX\nGXxmsMWWwsUvvJIj5x8iHU9osw+SMR0lmjDuohHFY9NTI1ywX1N1r3FmdmMNNTi4\naZHc6Z9omErhplmwv5FUbloiv1yUbcwhHwPVpMU/WsN2enXd7UCfg7sVo0wK7m3J\nRFXs1PkA2CFwfaebQyYDWuREoMzdPRb9H82SA/H7Lb5g876Tk79x4JP2NV5RmLGM\n3TpGp4KtGYiECLjHaTkToW6qIzzkwd5WAc4/84ILR1QcA572l8AQkBcD8ytMZEt8\n4hAjudebAgMBAAECggEBAJp+L0eLHmQ9aGNXro7OYMxElubYr4qbzHN3jOkmZctI\nqSNLQifdbcHpzs2JvuMryMT7V7+6embyIHEmOh1Y0XopcrQjKaNhjlApope0l5RA\nbYwNKNxHYKgoIFwJWQnpDSAWpY1EuFnjjl3lrJ1FUf6N9pupnjWRY+chAbEY3poi\n32Ls2ne+3EF6mKPD+yAWwnstW3ykfFa2y6EmOtziWnMusnVEEXLUYjBI10IqhwhR\nyE8WRgGiSzyGsL5rcRCuZ5HUxE9mSNk/J9daAbqFee/NlNsi14fTzdOvrcliCgAr\n4L3tYXZqnApd15gv1FyMGiR/VMfRUiWiQBNar8CNRcECgYEA6hmGDOHGzLJEc8f6\naP0ENwtG746S73SiSyTy2BRwh+gNSs0vq8Wka1wI6sElPrRVwPXSx4S17zvvH763\nMQ2+OqqgZPrkx0jeXNUWZlGMZ3aIOXFFDRem87tZSaxFEfmXc5FOomE2dmgGSF7i\n1E4d9Mwu1wWXYSMvu6dDXkWCo7ECgYEAwrT8+skTbOdFyW1PqSwQZhCGMtXM+69J\nTn+u53PoDPzXBZ51U4swFohViHyRnifIlfoyruEaxrULOKULefUVz/m7hgTbIKpj\nTAk9/G0lUHkLJJdKPqrtSq+w8vQpqCPaYTdlf/b+b8xyOUwG9TCEtllXujZsdJ9L\niAoCMFhofwsCgYB6gZDc/OoEBOY9kNFCT+X8yDH++yV5mhe0K0nKOigJdy49jtL7\nmRpJ9IfWEe1juwuFRx9eudxbrYmdmzhSu1ZpbREyxvkiMMfs3LY0JUjMfAMdzGDO\nUSpVMh2vqC8dEPhoygnUf/r4S8e956ncYGTczl1UuOBXPQqlsQpYMxgCgQKBgFE2\nvO6+QGQEc495EOk3f/+SlOdPVpkEnEcp6wKPzhLcw7OMTNP0ErLTWxn7G6IkZf5o\nxgs7ybdofK276fWMzPRa7mUQUXZmm9RzZm+L9yyB0KwKjuVk1mV4sw4j2dxQWB6E\nxMmDdM2dMWfE1oIfIrwMuBLr8IEUkKTFx/PybGPRAoGBAKi1pcQJBUCqyX5mKE47\nUdmPIlyRkwuWoUzV5GM0wIfcR+exf0V/HjdAInCfEbgnelFe1ea8IGZsscSbquUT\nndNWcCpwPIIo0oY+1jkS1/MM/xgT5u8afsnVziYtihODk2+qHas8glzcmNEWIKjc\nzpWAe2Ha6aV8GBcHkRvQ5+SC\n-----END PRIVATE KEY-----\n"
-	pubKey     = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgzY3njoy3aEe5DnYuQY\nkuQvH1b6C6E0pBBjZzSV3SYmdpj1xuWr9Qy1E/IL2ldDSZrpHhvwVxFB1xl8ZrDF\nlsLFL7ySI+cfIh1PaLMPkjEdJZow7qIRxWPTUyNcsF9Tda9xZnZjDTU4uGmR3Omf\naJhK4aZZsL+RVG5aIr9clG3MIR8D1aTFP1rDdnp13e1An4O7FaNMCu5tyURV7NT5\nANghcH2nm0MmA1rkRKDM3T0W/R/NkgPx+y2+YPO+k5O/ceCT9jVeUZixjN06RqeC\nrRmIhAi4x2k5E6FuqiM85MHeVgHOP/OCC0dUHAOe9pfAEJAXA/MrTGRLfOIQI7nX\nmwIDAQAB\n-----END PUBLIC KEY-----\n"
+	// priKey     = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCyDNjeeOjLdoR7\nkOdi5BiS5C8fVvoLoTSkEGNnNJXdJiZ2mPXG5av1DLUT8gvaV0NJmukeG/BXEUHX\nGXxmsMWWwsUvvJIj5x8iHU9osw+SMR0lmjDuohHFY9NTI1ywX1N1r3FmdmMNNTi4\naZHc6Z9omErhplmwv5FUbloiv1yUbcwhHwPVpMU/WsN2enXd7UCfg7sVo0wK7m3J\nRFXs1PkA2CFwfaebQyYDWuREoMzdPRb9H82SA/H7Lb5g876Tk79x4JP2NV5RmLGM\n3TpGp4KtGYiECLjHaTkToW6qIzzkwd5WAc4/84ILR1QcA572l8AQkBcD8ytMZEt8\n4hAjudebAgMBAAECggEBAJp+L0eLHmQ9aGNXro7OYMxElubYr4qbzHN3jOkmZctI\nqSNLQifdbcHpzs2JvuMryMT7V7+6embyIHEmOh1Y0XopcrQjKaNhjlApope0l5RA\nbYwNKNxHYKgoIFwJWQnpDSAWpY1EuFnjjl3lrJ1FUf6N9pupnjWRY+chAbEY3poi\n32Ls2ne+3EF6mKPD+yAWwnstW3ykfFa2y6EmOtziWnMusnVEEXLUYjBI10IqhwhR\nyE8WRgGiSzyGsL5rcRCuZ5HUxE9mSNk/J9daAbqFee/NlNsi14fTzdOvrcliCgAr\n4L3tYXZqnApd15gv1FyMGiR/VMfRUiWiQBNar8CNRcECgYEA6hmGDOHGzLJEc8f6\naP0ENwtG746S73SiSyTy2BRwh+gNSs0vq8Wka1wI6sElPrRVwPXSx4S17zvvH763\nMQ2+OqqgZPrkx0jeXNUWZlGMZ3aIOXFFDRem87tZSaxFEfmXc5FOomE2dmgGSF7i\n1E4d9Mwu1wWXYSMvu6dDXkWCo7ECgYEAwrT8+skTbOdFyW1PqSwQZhCGMtXM+69J\nTn+u53PoDPzXBZ51U4swFohViHyRnifIlfoyruEaxrULOKULefUVz/m7hgTbIKpj\nTAk9/G0lUHkLJJdKPqrtSq+w8vQpqCPaYTdlf/b+b8xyOUwG9TCEtllXujZsdJ9L\niAoCMFhofwsCgYB6gZDc/OoEBOY9kNFCT+X8yDH++yV5mhe0K0nKOigJdy49jtL7\nmRpJ9IfWEe1juwuFRx9eudxbrYmdmzhSu1ZpbREyxvkiMMfs3LY0JUjMfAMdzGDO\nUSpVMh2vqC8dEPhoygnUf/r4S8e956ncYGTczl1UuOBXPQqlsQpYMxgCgQKBgFE2\nvO6+QGQEc495EOk3f/+SlOdPVpkEnEcp6wKPzhLcw7OMTNP0ErLTWxn7G6IkZf5o\nxgs7ybdofK276fWMzPRa7mUQUXZmm9RzZm+L9yyB0KwKjuVk1mV4sw4j2dxQWB6E\nxMmDdM2dMWfE1oIfIrwMuBLr8IEUkKTFx/PybGPRAoGBAKi1pcQJBUCqyX5mKE47\nUdmPIlyRkwuWoUzV5GM0wIfcR+exf0V/HjdAInCfEbgnelFe1ea8IGZsscSbquUT\nndNWcCpwPIIo0oY+1jkS1/MM/xgT5u8afsnVziYtihODk2+qHas8glzcmNEWIKjc\nzpWAe2Ha6aV8GBcHkRvQ5+SC\n-----END PRIVATE KEY-----\n"
+	// pubKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgzY3njoy3aEe5DnYuQY\nkuQvH1b6C6E0pBBjZzSV3SYmdpj1xuWr9Qy1E/IL2ldDSZrpHhvwVxFB1xl8ZrDF\nlsLFL7ySI+cfIh1PaLMPkjEdJZow7qIRxWPTUyNcsF9Tda9xZnZjDTU4uGmR3Omf\naJhK4aZZsL+RVG5aIr9clG3MIR8D1aTFP1rDdnp13e1An4O7FaNMCu5tyURV7NT5\nANghcH2nm0MmA1rkRKDM3T0W/R/NkgPx+y2+YPO+k5O/ceCT9jVeUZixjN06RqeC\nrRmIhAi4x2k5E6FuqiM85MHeVgHOP/OCC0dUHAOe9pfAEJAXA/MrTGRLfOIQI7nX\nmwIDAQAB\n-----END PUBLIC KEY-----\n"
+
+	priKey = "-----BEGIN PRIVATE KEY-----\n" + "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCpY9nLOV8t8b4x\n" +
+		"lIAv4qwwiPeUz6LKReHCkbOs4nJaOvr1qvvMTcyKK3cdjB5v24VBnyFCQW5Ixvrh\n" +
+		"jrXytgb8UTJCiadd8rp+YECPEYZa0R7Jt9ul6aN9470VkL28mZcmzwSbd28L6mol\n" +
+		"syaRH3uu5Vy+57WnXiao+Xii4WKbxu7d0/GlfMbu27aegqbMUbPJRp9anDX+C8z8\n" +
+		"tW8VGfeVJ1cNDxhDuIrO68bBuxiB+zerhUfhpaDr7srVs0YWBNYGTn7dugPFaAYE\n" +
+		"40JFDa4brMkmoCFMTttBqY7UdUglHLEkT9XJfdBPSXWwg5rvJo2xtt+Xh3zZVURd\n" +
+		"cY/NzagDAgMBAAECggEAbrvq1hLUaPmfB1R4FFXPkQ8JIww19JuIgaS0W/HRJbFm\n" +
+		"/BDh+OZnL3BIt1UxVJiiXYrEuEaD7Sm/OpML2PYsTOhbvem1MxKJ3jHYIm8ncNlC\n" +
+		"kkYSXj0FdzfZFW8AynlxuZod/fAu9RAygiDCtp5pQaWJYveg5iADj/U+auSCjSiH\n" +
+		"qq9ZbB2SaBZti/GfEeLWngSsNtKocXuXqhEyS4xUSJUAdnER1ntsH/q3cKmtjk7+\n" +
+		"g0EGXfEgP4nm0qp9q7CmX5tK/ZqPnfcDCyMN3KBHo7cEnghuZ19waVBLW9j+uv83\n" +
+		"8JrIBYge5ntKDWiepmG2OQPLuWRvnF0TLJWxy3N3+QKBgQDcgiYlUYOZAOYriLvu\n" +
+		"BdmpMMdrvmaCgrAyz5kdQoh9DK6EQSgv8vhMvvU6696nnJywqiPqX3QcKz8BGZlg\n" +
+		"LTbXqjceuQF7aW1Zt9x+d+DN1cyQ9AhjNcVg5hI/YgAhAdva9UHJ7lUfw5/YLK2X\n" +
+		"M5EkliQDbr92lhSYkTjnQ2SDLwKBgQDEp2pxoc7GiO3/qTpU93qaDsIlxLeVsCrX\n" +
+		"xxVHTrm0lW5CQdbhSoj6q6N+Mo4oP5AtX9QqQ7mCaAlBDWAZlS8wActj17/npPOr\n" +
+		"gVoBv9Zq9C776A/+MTlCwlB1BmLaYQUykCY2cjnLWmgwQJqT703ASHdNrtkq54um\n" +
+		"5++Bo/vDbQKBgQCsl7IDw6mdOHbv3DY8N5gGNYfhbUYPIPuIybSukkUCm8p8+gLa\n" +
+		"hPKUH2MSm0vLJWn/XSx/ZfcblT3bPo4uGTWz2CcMhQID9qEAeEi9NFdgxNc5Hcjy\n" +
+		"3kN/dJUTx0ESlMHgv9ael01Jb3TNXysADfytBldp2GVEDHRSdlhzquwhQQKBgQCo\n" +
+		"6CrzAsm5mK7jIpUpmY7Cd96l1frhJPkHcMWEA8hZpOeZHTfVNdHFjFrW79FOHJpX\n" +
+		"frGaw6S4r2cTasuZ7ZskHsZ1MUBxVCq+qlGGoyElqCoaz828xMar4n58pUmOzDpM\n" +
+		"nadUqHOfiD1pBHRAkBA2EYf3PzDkOxCmARykOxbpWQKBgDwoWQipip4s62DXnNmI\n" +
+		"ffUnCBHKFJl3hlMwlujG87ip4++/qxHx4bcdgA9/JR9zBrIWX7kt6PWV/0aJviZi\n" +
+		"25panNe4jgXcTkrRUUUtJj7tiHsSl6OBZgyfyKoUayo2AzLB/gklsTA6NrIu7Rsw\n" +
+		"zsS126HwDrieYqQJj04C0mnU\n" +
+		"-----END PRIVATE KEY-----\n"
 )
 
 // getTime returns the current time formatted as a string
@@ -64,7 +92,7 @@ func getReqHeader() map[string]string {
 }
 
 // Retrieves the http request body information
-func getReqBody(funcName string, busParams map[string]string) map[string]string {
+func getReqBody(funcName string, busParams map[string]interface{}) map[string]string {
 	// Business parameters
 	busBytes, _ := json.Marshal(busParams)
 	busData := url.QueryEscape(string(busBytes))
@@ -82,19 +110,6 @@ func getReqBody(funcName string, busParams map[string]string) map[string]string 
 	// Constructing signature information
 	sign := buildSign(reqBody, secret)
 	reqBody["sign"] = sign
-	// Add rsa asymmetric algorithm signature
-	var r Rsa
-	r.SetPriKey([]byte(priKey))
-	r.SetCryptoHash(cryptoHash)
-	r.SetKeyFmt(keyFmt)
-	hashed, err := r.GenDataSum([]byte(sign))
-	// fmt.Printf("\n-------Data Hash:----------\n%s\n", hex.EncodeToString(hashed))
-	signature, err := r.RsaSignByHash(hashed)
-	if err != nil {
-		fmt.Println("Error signing message:", err)
-		return nil
-	}
-	reqBody["rsa_sign"] = base64.StdEncoding.EncodeToString(signature)
 	return reqBody
 }
 
@@ -130,21 +145,49 @@ func sendRequest(url, method string, reqHeader map[string]string, reqBody map[st
 	return nil, body
 }
 
+func genOrderData(orderNum, withdrawalAddress, userId, amount string) string {
+	return fmt.Sprintf("orderNum:%s,amount:%s,address:%s,userId:%s", orderNum, amount, withdrawalAddress, userId)
+}
+
 // Payment
-func Payment(from, to string, value float64, token string) (code, msg string) {
+func Payment(orderNum, withdrawalAddress, chainType, symbol, remark, userId, accId, acctKey, amount string, token string) (code, msg string) {
 	type Response struct {
 		Code  string
 		Msg   string
 		ReqId string
 	}
+	type AccReq struct {
+		AccId   string `json:"accId"`
+		AcctKey string `json:"acctKey"`
+	}
 	// api params
-	busParams := map[string]string{
-		"from":  from,
-		"to":    to,
-		"value": fmt.Sprintf("%f", value),
+	busParams := map[string]interface{}{
+		"orderNum":          orderNum,
+		"withdrawalAddress": withdrawalAddress,
+		"chainType":         chainType,
+		"symbol":            symbol,
+		"remark":            remark,
+		"amount":            amount,
+		"userId":            userId,
+		"accReq":            AccReq{AccId: accId, AcctKey: acctKey},
 	}
 
-	funcName := "assert.payment"
+	var r Rsa
+	r.SetPriKey([]byte(priKey))
+	r.SetCryptoHash(cryptoHash)
+	r.SetKeyFmt(keyFmt)
+	orderData := genOrderData(orderNum, withdrawalAddress, userId, amount)
+	hashed, err := r.GenDataSum([]byte(orderData))
+	// fmt.Printf("\n-------Data Hash:----------\n%s\n", hex.EncodeToString(hashed))
+	signature, err := r.RsaSignByHash(hashed)
+	if err != nil {
+		fmt.Println("Error signing message:", err)
+		return
+	}
+	busParams["rsaSign"] = base64.StdEncoding.EncodeToString(signature)
+	fmt.Printf("rsaSign:%s\n", busParams["rsaSign"])
+
+	funcName := "app.general.withdrawalOrderNo"
 	reqBody := getReqBody(funcName, busParams)
 	reqHeader := getReqHeader()
 	if "" != token {
@@ -167,10 +210,16 @@ func Payment(from, to string, value float64, token string) (code, msg string) {
 }
 
 func TestPayment(t *testing.T) {
-	from := ""
-	to := ""
-	value := 8.8888
-	code, msg := Payment(from, to, value, "")
+	orderNum := "W00000025"
+	withdrawalAddress := "TQggA8Gw7WaBi5ZmcCBdxtyjjbtHvGf9bN"
+	chainType := "tron"
+	symbol := "usdt"
+	remark := "remark test"
+	userId := "Ta123456"
+	accId := "YY20240500001"
+	acctKey := "2f206e50cdee40168fc8c0c133bac1a6"
+	amount := "66"
+	code, msg := Payment(orderNum, withdrawalAddress, chainType, symbol, remark, userId, accId, acctKey, amount, "")
 	if "0" == code {
 		fmt.Printf("payment successful, msg:%s\n", msg)
 	} else {
--- rsa/php/call_payment.php
+++ rsa/php/call_payment.php
@@ -3,13 +3,13 @@ require_once "Rsa.php";
 
 class Payment {
     // Constants and Settings
-    private $httpUrl = "";
+    private $httpUrl = "http://192.168.3.46:10011/interface/api";
     private $method = "POST";
-    private $appKey = "123456";
+    private $appKey = "test";
     private $secret = "123456";
-    private $privateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCyDNjeeOjLdoR7\nkOdi5BiS5C8fVvoLoTSkEGNnNJXdJiZ2mPXG5av1DLUT8gvaV0NJmukeG/BXEUHX\nGXxmsMWWwsUvvJIj5x8iHU9osw+SMR0lmjDuohHFY9NTI1ywX1N1r3FmdmMNNTi4\naZHc6Z9omErhplmwv5FUbloiv1yUbcwhHwPVpMU/WsN2enXd7UCfg7sVo0wK7m3J\nRFXs1PkA2CFwfaebQyYDWuREoMzdPRb9H82SA/H7Lb5g876Tk79x4JP2NV5RmLGM\n3TpGp4KtGYiECLjHaTkToW6qIzzkwd5WAc4/84ILR1QcA572l8AQkBcD8ytMZEt8\n4hAjudebAgMBAAECggEBAJp+L0eLHmQ9aGNXro7OYMxElubYr4qbzHN3jOkmZctI\nqSNLQifdbcHpzs2JvuMryMT7V7+6embyIHEmOh1Y0XopcrQjKaNhjlApope0l5RA\nbYwNKNxHYKgoIFwJWQnpDSAWpY1EuFnjjl3lrJ1FUf6N9pupnjWRY+chAbEY3poi\n32Ls2ne+3EF6mKPD+yAWwnstW3ykfFa2y6EmOtziWnMusnVEEXLUYjBI10IqhwhR\nyE8WRgGiSzyGsL5rcRCuZ5HUxE9mSNk/J9daAbqFee/NlNsi14fTzdOvrcliCgAr\n4L3tYXZqnApd15gv1FyMGiR/VMfRUiWiQBNar8CNRcECgYEA6hmGDOHGzLJEc8f6\naP0ENwtG746S73SiSyTy2BRwh+gNSs0vq8Wka1wI6sElPrRVwPXSx4S17zvvH763\nMQ2+OqqgZPrkx0jeXNUWZlGMZ3aIOXFFDRem87tZSaxFEfmXc5FOomE2dmgGSF7i\n1E4d9Mwu1wWXYSMvu6dDXkWCo7ECgYEAwrT8+skTbOdFyW1PqSwQZhCGMtXM+69J\nTn+u53PoDPzXBZ51U4swFohViHyRnifIlfoyruEaxrULOKULefUVz/m7hgTbIKpj\nTAk9/G0lUHkLJJdKPqrtSq+w8vQpqCPaYTdlf/b+b8xyOUwG9TCEtllXujZsdJ9L\niAoCMFhofwsCgYB6gZDc/OoEBOY9kNFCT+X8yDH++yV5mhe0K0nKOigJdy49jtL7\nmRpJ9IfWEe1juwuFRx9eudxbrYmdmzhSu1ZpbREyxvkiMMfs3LY0JUjMfAMdzGDO\nUSpVMh2vqC8dEPhoygnUf/r4S8e956ncYGTczl1UuOBXPQqlsQpYMxgCgQKBgFE2\nvO6+QGQEc495EOk3f/+SlOdPVpkEnEcp6wKPzhLcw7OMTNP0ErLTWxn7G6IkZf5o\nxgs7ybdofK276fWMzPRa7mUQUXZmm9RzZm+L9yyB0KwKjuVk1mV4sw4j2dxQWB6E\nxMmDdM2dMWfE1oIfIrwMuBLr8IEUkKTFx/PybGPRAoGBAKi1pcQJBUCqyX5mKE47\nUdmPIlyRkwuWoUzV5GM0wIfcR+exf0V/HjdAInCfEbgnelFe1ea8IGZsscSbquUT\nndNWcCpwPIIo0oY+1jkS1/MM/xgT5u8afsnVziYtihODk2+qHas8glzcmNEWIKjc\nzpWAe2Ha6aV8GBcHkRvQ5+SC\n-----END PRIVATE KEY-----\n";
-    private	$publicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgzY3njoy3aEe5DnYuQY\nkuQvH1b6C6E0pBBjZzSV3SYmdpj1xuWr9Qy1E/IL2ldDSZrpHhvwVxFB1xl8ZrDF\nlsLFL7ySI+cfIh1PaLMPkjEdJZow7qIRxWPTUyNcsF9Tda9xZnZjDTU4uGmR3Omf\naJhK4aZZsL+RVG5aIr9clG3MIR8D1aTFP1rDdnp13e1An4O7FaNMCu5tyURV7NT5\nANghcH2nm0MmA1rkRKDM3T0W/R/NkgPx+y2+YPO+k5O/ceCT9jVeUZixjN06RqeC\nrRmIhAi4x2k5E6FuqiM85MHeVgHOP/OCC0dUHAOe9pfAEJAXA/MrTGRLfOIQI7nX\nmwIDAQAB\n-----END PUBLIC KEY-----\n";
-
+    // private $privateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCyDNjeeOjLdoR7\nkOdi5BiS5C8fVvoLoTSkEGNnNJXdJiZ2mPXG5av1DLUT8gvaV0NJmukeG/BXEUHX\nGXxmsMWWwsUvvJIj5x8iHU9osw+SMR0lmjDuohHFY9NTI1ywX1N1r3FmdmMNNTi4\naZHc6Z9omErhplmwv5FUbloiv1yUbcwhHwPVpMU/WsN2enXd7UCfg7sVo0wK7m3J\nRFXs1PkA2CFwfaebQyYDWuREoMzdPRb9H82SA/H7Lb5g876Tk79x4JP2NV5RmLGM\n3TpGp4KtGYiECLjHaTkToW6qIzzkwd5WAc4/84ILR1QcA572l8AQkBcD8ytMZEt8\n4hAjudebAgMBAAECggEBAJp+L0eLHmQ9aGNXro7OYMxElubYr4qbzHN3jOkmZctI\nqSNLQifdbcHpzs2JvuMryMT7V7+6embyIHEmOh1Y0XopcrQjKaNhjlApope0l5RA\nbYwNKNxHYKgoIFwJWQnpDSAWpY1EuFnjjl3lrJ1FUf6N9pupnjWRY+chAbEY3poi\n32Ls2ne+3EF6mKPD+yAWwnstW3ykfFa2y6EmOtziWnMusnVEEXLUYjBI10IqhwhR\nyE8WRgGiSzyGsL5rcRCuZ5HUxE9mSNk/J9daAbqFee/NlNsi14fTzdOvrcliCgAr\n4L3tYXZqnApd15gv1FyMGiR/VMfRUiWiQBNar8CNRcECgYEA6hmGDOHGzLJEc8f6\naP0ENwtG746S73SiSyTy2BRwh+gNSs0vq8Wka1wI6sElPrRVwPXSx4S17zvvH763\nMQ2+OqqgZPrkx0jeXNUWZlGMZ3aIOXFFDRem87tZSaxFEfmXc5FOomE2dmgGSF7i\n1E4d9Mwu1wWXYSMvu6dDXkWCo7ECgYEAwrT8+skTbOdFyW1PqSwQZhCGMtXM+69J\nTn+u53PoDPzXBZ51U4swFohViHyRnifIlfoyruEaxrULOKULefUVz/m7hgTbIKpj\nTAk9/G0lUHkLJJdKPqrtSq+w8vQpqCPaYTdlf/b+b8xyOUwG9TCEtllXujZsdJ9L\niAoCMFhofwsCgYB6gZDc/OoEBOY9kNFCT+X8yDH++yV5mhe0K0nKOigJdy49jtL7\nmRpJ9IfWEe1juwuFRx9eudxbrYmdmzhSu1ZpbREyxvkiMMfs3LY0JUjMfAMdzGDO\nUSpVMh2vqC8dEPhoygnUf/r4S8e956ncYGTczl1UuOBXPQqlsQpYMxgCgQKBgFE2\nvO6+QGQEc495EOk3f/+SlOdPVpkEnEcp6wKPzhLcw7OMTNP0ErLTWxn7G6IkZf5o\nxgs7ybdofK276fWMzPRa7mUQUXZmm9RzZm+L9yyB0KwKjuVk1mV4sw4j2dxQWB6E\nxMmDdM2dMWfE1oIfIrwMuBLr8IEUkKTFx/PybGPRAoGBAKi1pcQJBUCqyX5mKE47\nUdmPIlyRkwuWoUzV5GM0wIfcR+exf0V/HjdAInCfEbgnelFe1ea8IGZsscSbquUT\nndNWcCpwPIIo0oY+1jkS1/MM/xgT5u8afsnVziYtihODk2+qHas8glzcmNEWIKjc\nzpWAe2Ha6aV8GBcHkRvQ5+SC\n-----END PRIVATE KEY-----\n";
+    // private	$publicKey = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgzY3njoy3aEe5DnYuQY\nkuQvH1b6C6E0pBBjZzSV3SYmdpj1xuWr9Qy1E/IL2ldDSZrpHhvwVxFB1xl8ZrDF\nlsLFL7ySI+cfIh1PaLMPkjEdJZow7qIRxWPTUyNcsF9Tda9xZnZjDTU4uGmR3Omf\naJhK4aZZsL+RVG5aIr9clG3MIR8D1aTFP1rDdnp13e1An4O7FaNMCu5tyURV7NT5\nANghcH2nm0MmA1rkRKDM3T0W/R/NkgPx+y2+YPO+k5O/ceCT9jVeUZixjN06RqeC\nrRmIhAi4x2k5E6FuqiM85MHeVgHOP/OCC0dUHAOe9pfAEJAXA/MrTGRLfOIQI7nX\nmwIDAQAB\n-----END PUBLIC KEY-----\n";
+    private $privateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCpY9nLOV8t8b4x\nlIAv4qwwiPeUz6LKReHCkbOs4nJaOvr1qvvMTcyKK3cdjB5v24VBnyFCQW5Ixvrh\njrXytgb8UTJCiadd8rp+YECPEYZa0R7Jt9ul6aN9470VkL28mZcmzwSbd28L6mol\nsyaRH3uu5Vy+57WnXiao+Xii4WKbxu7d0/GlfMbu27aegqbMUbPJRp9anDX+C8z8\ntW8VGfeVJ1cNDxhDuIrO68bBuxiB+zerhUfhpaDr7srVs0YWBNYGTn7dugPFaAYE\n40JFDa4brMkmoCFMTttBqY7UdUglHLEkT9XJfdBPSXWwg5rvJo2xtt+Xh3zZVURd\ncY/NzagDAgMBAAECggEAbrvq1hLUaPmfB1R4FFXPkQ8JIww19JuIgaS0W/HRJbFm\n/BDh+OZnL3BIt1UxVJiiXYrEuEaD7Sm/OpML2PYsTOhbvem1MxKJ3jHYIm8ncNlC\nkkYSXj0FdzfZFW8AynlxuZod/fAu9RAygiDCtp5pQaWJYveg5iADj/U+auSCjSiH\nqq9ZbB2SaBZti/GfEeLWngSsNtKocXuXqhEyS4xUSJUAdnER1ntsH/q3cKmtjk7+\ng0EGXfEgP4nm0qp9q7CmX5tK/ZqPnfcDCyMN3KBHo7cEnghuZ19waVBLW9j+uv83\n8JrIBYge5ntKDWiepmG2OQPLuWRvnF0TLJWxy3N3+QKBgQDcgiYlUYOZAOYriLvu\nBdmpMMdrvmaCgrAyz5kdQoh9DK6EQSgv8vhMvvU6696nnJywqiPqX3QcKz8BGZlg\nLTbXqjceuQF7aW1Zt9x+d+DN1cyQ9AhjNcVg5hI/YgAhAdva9UHJ7lUfw5/YLK2X\nM5EkliQDbr92lhSYkTjnQ2SDLwKBgQDEp2pxoc7GiO3/qTpU93qaDsIlxLeVsCrX\nxxVHTrm0lW5CQdbhSoj6q6N+Mo4oP5AtX9QqQ7mCaAlBDWAZlS8wActj17/npPOr\ngVoBv9Zq9C776A/+MTlCwlB1BmLaYQUykCY2cjnLWmgwQJqT703ASHdNrtkq54um\n5++Bo/vDbQKBgQCsl7IDw6mdOHbv3DY8N5gGNYfhbUYPIPuIybSukkUCm8p8+gLa\nhPKUH2MSm0vLJWn/XSx/ZfcblT3bPo4uGTWz2CcMhQID9qEAeEi9NFdgxNc5Hcjy\n3kN/dJUTx0ESlMHgv9ael01Jb3TNXysADfytBldp2GVEDHRSdlhzquwhQQKBgQCo\n6CrzAsm5mK7jIpUpmY7Cd96l1frhJPkHcMWEA8hZpOeZHTfVNdHFjFrW79FOHJpX\nfrGaw6S4r2cTasuZ7ZskHsZ1MUBxVCq+qlGGoyElqCoaz828xMar4n58pUmOzDpM\nnadUqHOfiD1pBHRAkBA2EYf3PzDkOxCmARykOxbpWQKBgDwoWQipip4s62DXnNmI\nffUnCBHKFJl3hlMwlujG87ip4++/qxHx4bcdgA9/JR9zBrIWX7kt6PWV/0aJviZi\n25panNe4jgXcTkrRUUUtJj7tiHsSl6OBZgyfyKoUayo2AzLB/gklsTA6NrIu7Rsw\nzsS126HwDrieYqQJj04C0mnU\n-----END PRIVATE KEY-----\n";
     // Returns current time in a specific format
     function getTime() {
         return date("Y-m-d H:i:s");
@@ -41,34 +41,29 @@ class Payment {
     // Retrieves the HTTP request body information
     function getReqBody($funcName, $busParams) {
         $busData = urlencode(json_encode($busParams));
-        $sign = $this->buildSign($busParams, $this->secret);
         $reqBody = [
             "name" => $funcName,
             "app_key" => $this->appKey,
             "data" => $busData,
             "timestamp" => $this->getTime(),
             "version" => "1.0",
-            "access_token" => "",
-            "sign" => $sign
+            "access_token" => ""
         ];
-
-        // Add RSA signature
-        $rsa = new Rsa();
-        $rsa->setPriKey($this->privateKey);
-        $rsa->setPubKey($this->publicKey);
-        $rsa->setCryptoHash(OPENSSL_ALGO_SHA256);
-        $rsa->setKeyFmt("PKCS#8");
-
-        $reqBody['rsa_sign'] = $rsa->rsaSign($sign);
+        $sign = $this->buildSign($reqBody, $this->secret);
+        $reqBody['sign'] = $sign;
         return $reqBody;
     }
 
     // Sends HTTP request to the service
     function sendRequest($url, $method, $headers, $body) {
+
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',               // 设置发送内容类型为JSON
+            'Content-Length: ' . strlen(json_encode($body))          // 设置内容长度
+        ));
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
@@ -80,15 +75,38 @@ class Payment {
         return [null, $response];
     }
 
+    function genOrderData($orderNum, $withdrawalAddress, $userId, $amount) {
+        return sprintf("orderNum:%s,amount:%s,address:%s,userId:%s", $orderNum, $amount, $withdrawalAddress, $userId);
+    }
+
     // Main Functionality: Payment
-    function Payment($from, $to, $value, $token = "") {
+    function Payment($orderNum, $withdrawalAddress, $chainType, $symbol, $remark, $amount, $userId, $accId, $acctKey, $token = "") {
         $busParams = [
-            "from" => $from,
-            "to" => $to,
-            "value" => number_format($value, 4, '.', '')
+            "orderNum" =>        $orderNum,
+            "withdrawalAddress" => $withdrawalAddress,
+            "chainType" =>         $chainType,
+            "symbol" =>            $symbol,
+            "remark" =>           $remark,
+            "amount" =>           $amount,
+            "userId" =>          $userId,
+            "accReq" =>          [
+                "accId" =>  $accId,
+                "acctKey" => $acctKey
+            ],
         ];
 
-        $funcName = "assert.payment";
+        // Add RSA signature
+        $rsa = new Rsa();
+        $rsa->setPriKey($this->privateKey);
+        // $rsa->setPubKey($this->publicKey);
+        $rsa->setCryptoHash(OPENSSL_ALGO_SHA256);
+        $rsa->setKeyFmt("PKCS#8");
+        $orderData = $this->genOrderData($orderNum, $withdrawalAddress, $userId, $amount);
+        // echo "orderData:" . $orderData . "\n";
+        $busParams['rsaSign'] = $rsa->rsaSign($orderData);
+        // echo "rsaSign:" . $busParams['rsaSign'] . "\n";
+
+        $funcName = "app.general.withdrawalOrderNo";
         $reqBody = $this->getReqBody($funcName, $busParams);
         $reqHeader = $this->getReqHeader();
         if (!empty($token)) {
@@ -99,16 +117,24 @@ class Payment {
             return ['-1', $error];
         }
         $res = json_decode($resBody, true);
-        return [$res['Code'], $res['Msg']];
+        return [$res['code'], $res['msg']];
+        // return [$res];
     }
 }
 
 // Example usage
 $p = new Payment();
-$from = "";
-$to = "";
-$value = 8.8888;
-$result = $p->Payment($from, $to, $value, "");
+$orderNum = "W00000025";
+$withdrawalAddress = "TQggA8Gw7WaBi5ZmcCBdxtyjjbtHvGf9bN";
+$chainType = "tron";
+$symbol = "usdt";
+$remark = "remark test";
+$userId = "Ta123456";
+$accId = "YY20240500001";
+$acctKey = "2f206e50cdee40168fc8c0c133bac1a6";
+$amount = "66";
+
+$result = $p->Payment($orderNum, $withdrawalAddress, $chainType, $symbol, $remark, $amount, $userId, $accId, $acctKey, "");
 $code = $result[0];
 $msg = $result[1];
 if ("0" == $code) {
